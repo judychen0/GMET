9713afe2745dafe196586c1e053dacf1db49f247
Judy Chen <judy453378@gmail.com>
2021-04-26 13:49:28 +0800

    add dir Draw

---
+++ Draw/xDraw.C
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <fstream>
+#include "TH1F.h"
+using namespace std;
+
+void xDraw(){
+  //TFile *fopen = new TFile("output_ggtree_mc.root");
+  ofstream ftext;
+
+  TString rootname = "./output_ggtree.root";
+  TFile *fopen = new TFile(rootname);
+  TCanvas *c1 = new TCanvas("c1");
+  TTree *t =(TTree*)fopen->Get("t");
+
+  const char *title;
+  //const char *saveto = "/home/judy/ntuhep/GJet/summer16_Zg_EWK/"; 
+  const char *saveto = "./graph"; 
+  //draw TH1F non-logY histograms
+  TH1F *H_npho = (TH1F*)fopen->Get("h_npho");
+  H_npho->GetXaxis()->SetTitle("photon number");
+  H_npho->GetYaxis()->SetTitle("Events");
+  H_npho->Draw();
+  title = H_npho->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TH1F *H_nmcpho = (TH1F*)fopen->Get("h_nmcpho");
+  H_nmcpho->Draw();
+  title = H_nmcpho->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_matchpho = (TH1F*)fopen->Get("h_matchpho");
+  H_matchpho->Draw();
+  title = H_matchpho->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_nrealphoEB = (TH1F*)fopen->Get("h_nrealphoEB");
+  H_nrealphoEB->Draw();
+  title = H_nrealphoEB->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  Int_t nbins;
+  nbins = H_nmcpho->GetNbinsX();
+  Double_t mcpho_count = H_nmcpho->Integral(2, nbins);
+  nbins = H_matchpho->GetNbinsX();
+  Double_t matchpho_count = H_matchpho->Integral(2, nbins);
+  Double_t genmatch_eff = (matchpho_count/mcpho_count);
+  
+  cout << "gen-matched eff : " << genmatch_eff << endl;
+  ftext.open("eff.txt", ios::out | ios::app);
+  if(!ftext){
+    cerr << "Can't open file !" << endl;
+    exit(1);
+  }
+  ftext <<  "gen-matched eff : " << genmatch_eff << endl;
+  ftext.close();
+
+  nbins = H_nrealphoEB->GetNbinsX();
+  Double_t nrealphoEB_count = H_nrealphoEB->Integral(2, nbins);
+  Double_t MIDcut_eff = (nrealphoEB_count/matchpho_count);
+
+  cout << "M-ID cut eff : " << MIDcut_eff << endl;
+  ftext.open("eff.txt", ios::out | ios::app);
+  if(!ftext){
+    cerr << "Can't open file !" << endl;
+    exit(1);
+  }
+  ftext <<  "M-ID cut eff : " << MIDcut_eff << endl;
+  ftext.close();
+  
+  
+  Double_t ptcut[30] = {22, 30, 36, 50, 75, 90, 120, 170, 175, 180, 185, 190, 200,
+		     210, 220, 230, 250, 300, 350, 400, 500, 750, 1000, 1500, 2000, 3000, 10000};//24 bins, 2016
+
+  
+  TH1F *H_mcphoEta = new TH1F("H_mcphoEta", "H_mcphoEta", 60, -3, 3);
+  t->Draw("mcphoEta>>H_mcphoEta");
+  title = H_mcphoEta->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_mcphoPhi = new TH1F("H_mcphoPhi", "H_mcphoPhi", 8, -3.14, 3.14);
+  t->Draw("mcphoPhi>>H_mcphoPhi");
+  title = H_mcphoPhi->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TH1F *H_realphoEta = new TH1F("H_realphoEta", "H_realphoEta", 60, -3, 3);
+  t->Draw("realphoEta>>H_realphoEta");
+  title = H_realphoEta->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_realphoPhi = new TH1F("H_realphoPhi", "H_realphoPhi", 8, -3.14, 3.14);
+  t->Draw("realphoPhi>>H_realphoPhi");
+  title = H_realphoPhi->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  c1->SetLogy();
+  c1->Update();
+
+  TH1F *H_mcphoEt = new TH1F("H_mcphoEt", "H_mcphoEt", 24, ptcut);
+  t->Draw("mcphoEt>>H_mcphoEt");
+  title = H_mcphoEt->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TH1F *H_realphoEt = new TH1F("H_realphoEt", "H_realphoEt", 24, ptcut);
+  t->Draw("realphoEt>>H_realphoEt");
+  title = H_realphoEt->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_genMET = (TH1F*)fopen->Get("h_genMET");
+  H_genMET->Draw();
+  title = H_genMET->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TH1F *H_genMETPhi = (TH1F*)fopen->Get("h_genMETPhi");
+  H_genMETPhi->Draw();
+  title = H_genMETPhi->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_MET = (TH1F*)fopen->Get("h_MET");
+  H_MET->Draw();
+  title = H_MET->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_METPhi = (TH1F*)fopen->Get("h_METPhi");
+  H_METPhi->Draw();
+  title = H_METPhi->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TH1F *H_dr_pho = (TH1F*)fopen->Get("h_dr_pho");
+  H_dr_pho->Draw();
+  title = H_dr_pho->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_dpt_pho = (TH1F*)fopen->Get("h_dpt_pho");
+  H_dpt_pho->Draw();
+  title = H_dpt_pho->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  
+  TH1F *H_dptdr_pho = (TH1F*)fopen->Get("h_dptdr_pho");
+  c1->SetLogz(); c1->Update();
+  H_dptdr_pho->GetXaxis()->SetTitle("dr");
+  H_dptdr_pho->GetYaxis()->SetTitle("dpt");
+  H_dptdr_pho->Draw("colz");
+  title = H_dptdr_pho->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  c1->SetLogz(0);
+  c1->SetLogy(0);
+  c1->Update();
+  
+  
+  /*
+  TH1F *H_jetCEF = new TH1F("H_jetCEF", "H_jetCEF", 40, 0, 1);
+  H_jetCEF->GetYaxis()->SetRangeUser(.01, 1000000);
+  t->Draw("jetCEF>>H_jetCEF");
+  title = H_jetCEF->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_jetNEF = new TH1F("H_jetNEF", "H_jetNEF", 40, 0, 1);
+  H_jetNEF->GetYaxis()->SetRangeUser(.01, 1000000);
+  t->Draw("jetNEF>>H_jetCE");
+  title = H_jetNEF->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_jetCHF = new TH1F("H_jetCHF", "H_jetCHF", 40, 0, 1);
+  H_jetCHF->GetYaxis()->SetRangeUser(.01, 1000000);
+  t->Draw("jetCHF>>H_jetCHF");
+  title = H_jetCHF->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+
+  TH1F *H_jetNHF = new TH1F("H_jetNHF", "H_jetNHF", 40, 0, 1);
+  H_jetNHF->GetYaxis()->SetRangeUser(.01, 1000000);
+  t->Draw("jetNHF>>H_jetNHF");
+  title = H_jetNHF->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TH1F *H_jetNCH = new TH1F("H_jetNCH", "H_jetNCH", 100, 0, 100);
+  H_jetNCH->GetYaxis()->SetRangeUser(.01, 1000000);
+  t->Draw("jetNCH>>H_jetNCH");
+  title = H_jetNCH->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TH1F *H_jetNNP = new TH1F("H_jetNNP", "H_jetNNP", 100, 0, 100);
+  H_jetNNP->GetYaxis()->SetRangeUser(.01, 1000000);
+  t->Draw("jetNNP>>H_jetNNP");
+  title = H_jetNNP->GetName();
+  c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  */
+  c1->SetLogy(0);
+  //c1->SetLogz();
+  c1->Update();
+}
---
+++ Draw/xDraw_phoIDeff.C
@@ -0,0 +1,165 @@
+#include <iostream>
+#include <fstream>
+#include "TH1F.h"
+using namespace std;
+
+void xDraw_phoIDeff(){
+  ofstream ftext;
+  
+  TString rootname = "./output_ggtree.root";
+  TFile *fopen = new TFile(rootname);
+  TCanvas *c1 = new TCanvas("c1");
+  TTree *t =(TTree*)fopen->Get("t");
+
+  const char *title;
+  //const char *saveto = "/home/judy/ntuhep/GJet/summer16_Zg_EWK/"; 
+  const char *saveto = "./graph";
+
+  TH1F *H_realphoEB_pt_var = (TH1F*)fopen->Get("h_realphoEB_pt_var");
+  TH1F *H_realphoEB_pt_L_var = (TH1F*)fopen->Get("h_realphoEB_pt_L_var");
+  TH1F *H_realphoEB_pt_M_var = (TH1F*)fopen->Get("h_realphoEB_pt_M_var");
+  TH1F *H_realphoEB_pt_T_var = (TH1F*)fopen->Get("h_realphoEB_pt_T_var");
+
+  TGraphAsymmErrors *tgrs_L = new TGraphAsymmErrors();
+  tgrs_L->Divide(H_realphoEB_pt_L_var, H_realphoEB_pt_var);
+  tgrs_L->SetNameTitle("H_phocuteff_L", "pho Loose cut eff");
+  tgrs_L->SetMarkerStyle(8);
+  tgrs_L->SetMarkerColor(8);
+  //tgrs_L->SetMarkerSize(8);
+  tgrs_L->Draw("AP");
+  title = tgrs_L->GetName();
+  //c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TGraphAsymmErrors *tgrs_M = new TGraphAsymmErrors();
+  tgrs_M->Divide(H_realphoEB_pt_M_var, H_realphoEB_pt_var);
+  tgrs_M->SetNameTitle("H_phocuteff_M", "pho Medium cut eff");
+  tgrs_M->SetMarkerStyle(8);
+  tgrs_M->SetMarkerColor(8);
+  //tgrs_M->SetMarkerSize(8);
+  tgrs_M->Draw("AP");
+  title = tgrs_M->GetName();
+  //c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TGraphAsymmErrors *tgrs_T = new TGraphAsymmErrors();
+  tgrs_T->Divide(H_realphoEB_pt_T_var, H_realphoEB_pt_var);
+  tgrs_T->SetNameTitle("H_phocuteff_T", "pho Tight cut eff");
+  tgrs_T->SetMarkerStyle(8);
+  tgrs_T->SetMarkerColor(8);
+  //tgrs_T->SetMarkerSize(8);
+  tgrs_T->Draw("AP");
+  title = tgrs_T->GetName();
+  //c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  //get L/M/T cut eff multi-histo
+
+  Int_t turnpoint = 8;//point count from 0; EWK 2; Znunug 8
+  Double_t x, y, ex, ey; 
+  
+  ftext.open("eff.txt", ios::out | ios::app);
+  if(!ftext){
+    cerr << "Can't open file !" << endl;
+    exit(1);
+  }
+  tgrs_L->GetPoint(turnpoint, x, y);
+  ey = tgrs_L->GetErrorY(turnpoint);
+  ftext << "phoID L turn up point: " << x << " efficiency: " << y << " error: " << ey << endl;
+  
+  tgrs_M->GetPoint(turnpoint, x, y);
+  ey = tgrs_M->GetErrorY(turnpoint);
+  ftext << "phoID M turn up point: " << x << " efficiency: " << y << " error: " << ey << endl;
+
+  tgrs_T->GetPoint(turnpoint, x, y);
+  ey = tgrs_T->GetErrorY(turnpoint);
+  ftext << "phoID T turn up point: " << x << " efficiency: " << y << " error: " << ey << endl;
+  
+  ftext.close();
+  
+  
+  TMultiGraph *mg = new TMultiGraph();
+  
+  tgrs_L->SetMarkerColor(kGreen+1);
+  //tgrs_L->Draw("AP");
+  tgrs_M->SetMarkerColor(kAzure+1);
+  //tgrs_M->Draw("AP SAME");
+  tgrs_T->SetMarkerColor(kYellow);
+  //tgrs_T->Draw("AP SAME");
+  mg->Add(tgrs_L);
+  mg->Add(tgrs_M);
+  mg->Add(tgrs_T);
+  mg->GetXaxis()->SetTitle("phoEt#[]{GeV}");
+  mg->GetYaxis()->SetTitle("Efficiency");
+  mg->GetXaxis()->SetLimits(-1, 500);
+  mg->SetMaximum(1.05);
+  mg->Draw("AP");
+
+  TLegend *legend=new TLegend(0.15,0.75, 0.25, 0.88);
+  legend->SetBorderSize(0);
+  legend->SetFillColor(0);
+  legend->AddEntry(tgrs_L, "Loose", "P");
+  legend->AddEntry(tgrs_M, "Medium", "P");
+  legend->AddEntry(tgrs_T, "Tight", "P");
+  legend->Draw("SAME");
+  c1->SaveAs(Form("%s/%s.pdf",saveto, "h_realphoEB_cuteff"));
+
+  TH1F *H_realphoEB_eta_var = (TH1F*)fopen->Get("h_realphoEB_eta_var");
+  TH1F *H_realphoEB_eta_L_var = (TH1F*)fopen->Get("h_realphoEB_eta_L_var");
+  TH1F *H_realphoEB_eta_M_var = (TH1F*)fopen->Get("h_realphoEB_eta_M_var");
+  TH1F *H_realphoEB_eta_T_var = (TH1F*)fopen->Get("h_realphoEB_eta_T_var");
+
+  TGraphAsymmErrors *tgrs_eta_L = new TGraphAsymmErrors();
+  tgrs_eta_L->Divide(H_realphoEB_eta_L_var, H_realphoEB_eta_var);
+  tgrs_eta_L->SetNameTitle("H_phocuteff_eta_L", "pho Loose cut eff");
+  tgrs_eta_L->SetMarkerStyle(8);
+  tgrs_eta_L->SetMarkerColor(8);
+  //tgrs_eta_L->SetMarkerSize(8);
+  tgrs_eta_L->Draw("AP");
+  title = tgrs_eta_L->GetName();
+  //c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TGraphAsymmErrors *tgrs_eta_M = new TGraphAsymmErrors();
+  tgrs_eta_M->Divide(H_realphoEB_eta_M_var, H_realphoEB_eta_var);
+  tgrs_eta_M->SetNameTitle("H_phocuteff_eta_M", "pho Medium cut eff");
+  tgrs_eta_M->SetMarkerStyle(8);
+  tgrs_eta_M->SetMarkerColor(8);
+  //tgrs_eta_M->SetMarkerSize(8);
+  tgrs_eta_M->Draw("AP");
+  title = tgrs_eta_M->GetName();
+  //c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  
+  TGraphAsymmErrors *tgrs_eta_T = new TGraphAsymmErrors();
+  tgrs_eta_T->Divide(H_realphoEB_eta_T_var, H_realphoEB_eta_var);
+  tgrs_eta_T->SetNameTitle("H_phocuteff_eta_T", "pho Tight cut eff");
+  tgrs_eta_T->SetMarkerStyle(8);
+  tgrs_eta_T->SetMarkerColor(8);
+  //tgrs_eta_T->SetMarkerSize(8);
+  tgrs_eta_T->Draw("AP");
+  title = tgrs_eta_T->GetName();
+  //c1->SaveAs(Form("%s/%s.pdf", saveto, title));
+  //get L/M/T cut eff multi-histo
+  
+  TMultiGraph *mg1 = new TMultiGraph();
+  
+  tgrs_eta_L->SetMarkerColor(kGreen+1);
+  //tgrs_eta_L->Draw("AP");
+  tgrs_eta_M->SetMarkerColor(kAzure+1);
+  //tgrs_eta_M->Draw("AP SAME");
+  tgrs_eta_T->SetMarkerColor(kYellow);
+  //tgrs_eta_T->Draw("AP SAME");
+  mg1->Add(tgrs_eta_L);
+  mg1->Add(tgrs_eta_M);
+  mg1->Add(tgrs_eta_T);
+  mg1->GetXaxis()->SetTitle("phoEta#[]{GeV}");
+  mg1->GetYaxis()->SetTitle("Efficiency");
+  mg1->GetXaxis()->SetLimits(-3, 3);
+  mg1->SetMaximum(1.05);
+  mg1->SetMinimum(0.);
+  mg1->Draw("AP");
+
+  TLegend *legend1=new TLegend(0.15,0.75, 0.25, 0.88);
+  legend1->SetBorderSize(0);
+  legend1->SetFillColor(0);
+  legend1->AddEntry(tgrs_eta_L, "Loose", "P");
+  legend1->AddEntry(tgrs_eta_M, "Medium", "P");
+  legend1->AddEntry(tgrs_eta_T, "Tight", "P");
+  legend1->Draw("SAME");
+  c1->SaveAs(Form("%s/%s.pdf",saveto, "h_realphoEB_cuteff_eta"));
+}
