3edfa5c499213548f49e15cbf3bd1a77c98393da
Judy Chen <judy453378@gmail.com>
2021-05-12 18:40:09 +0800

    4 sample stack plot

--- Draw/xDrawStack.C
+++ Draw/xDrawStack.C
@@ -4,16 +4,16 @@
 #include <string>
 #include "TH1F.h"
 using namespace std;
-#define nfile 4
+#define nfile 3
 #define eqevents 1000000
 
 void xDrawStack(){
-  TString rootname[5] = {"/home/judy/ntuhep/GMET/output_file/summer16/mc/210505/summer16_GJet_Pt-15To6000_TuneCUETP8M1-Flat_13TeV_pythia8_20M_0000/210505_040352/output_ggtree.root",
-			 "/home/judy/ntuhep/GMET/output_file/summer16/mc/210505/summer16_WGToLNuG_01J_5f_PtG_130_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_0000/210505_082513/output_ggtree.root",
-			 "/home/judy/ntuhep/GMET/output_file/summer16/mc/210505/summer16_ZLLGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph_0000/210505_064501/output_ggtree.root",
-			 "/home/judy/ntuhep/GMET/output_file/summer16/mc/210505/summer16_ZGTo2NuG_PtG-130_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_0000/210505_064756/output_ggtree.root"
+  TString rootname[5] = {"/home/judy/ntuhep/GMET/output_file/summer16/mc/summer16_GJet_Pt-20toInf_DoubleEMEnriched_MGG-40to80_TuneCUETP8M1_13TeV_Pythia8_0000/210511_103920/output_ggtree.root",
+			 "/home/judy/ntuhep/GMET/output_file/summer16/mc/summer16_WGToLNuG_01J_5f_PtG_130_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_0000/210510_131544/output_ggtree.root",
+			 "/home/judy/ntuhep/GMET/output_file/summer16/mc/summer16_ZLLGJets_MonoPhoton_PtG-130_TuneCUETP8M1_13TeV-madgraph_0000/210511_103619/output_ggtree.root",
+			 "/home/judy/ntuhep/GMET/output_file/summer16/mc/summer16_ZGTo2NuG_PtG-130_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_0000/210511_103054/output_ggtree.root"
   };//[0,1,2,3][GJet,WG, ZG,ZNuNu]
-  Float_t mclumi[5] = {0.002744,311.6,2.786,583.9};//(fb-1) [0,1,2,3,4][GJet,WG,ZG,ZNuNu]
+  Float_t mclumi[5] = {0.31,311.6,6750,989.7};//(fb-1) [0,1,2,3,4][GJet,WG,ZG,ZNuNu]
   //Float_t mccross[5] = {}
   Float_t lumi16 = 36.33;
 
@@ -23,6 +23,15 @@ void xDrawStack(){
   string hexcolor[8] = {"#7FB5F5","#2F81A3","#1C2A60","#FA6D5F","#91A81E","#DCF563", "#1C6040","#EFE2C9"};
   TColor *color[8];
   Int_t cnum[8];
+   
+  for(Int_t j=0; j<8; j++){
+    color[j] = new TColor();
+    Int_t n = hexcolor[j].length();
+    char chararray[n+1];
+    strcpy(chararray, hexcolor[j].c_str());
+    cnum[j] = color[j]->GetColor(chararray);
+  } 
+    
   
   TFile *fopen;
   TCanvas *c1 = new TCanvas("c1");
@@ -35,7 +44,6 @@ void xDrawStack(){
   Double_t cutmin = -1.6;
   Double_t cutmax  = 1.6;
   
- 
   TH1F *H_MET[nfile];
   TH1F *H_dphoMETPhi[nfile];
   TH1F *H_phoEB_pt_MET[nfile];
@@ -55,7 +63,7 @@ void xDrawStack(){
     outentries = eqevents*(lumi16/mclumi[i]);
     scale = outentries/entries;
     //if(i==4) scale*=100000;
-    if(i==0) scale*=0.00001;
+    //if(i==0) scale*=0.00001;
     
     H_MET[i] = (TH1F*)fopen->Get("h_MET");
     H_MET[i]->Scale(scale);
@@ -73,15 +81,7 @@ void xDrawStack(){
     H_phoEB_ptoverMET[i]->Scale(scale);
     //fopen->Close();
 
-    
-    for(Int_t j=0; j<8; j++){
-      color[j] = new TColor();
-      Int_t n = hexcolor[j].length();
-      char chararray[n+1];
-      strcpy(chararray, hexcolor[j].c_str());
-      cnum[j] = color[j]->GetColor(chararray);
-    } 
-    
+   
     H_MET[i]->SetFillColor(cnum[i]);
     H_MET[i]->SetLineColor(cnum[i]);
 
@@ -149,18 +149,50 @@ void xDrawStack(){
     title = H_phoEB_ptoverMET[i]->GetName();
     //c1->SaveAs(Form("%s/%s%i.pdf", saveto, title, i));
   }
+
+  TLegend *lhs = new TLegend(0.65,0.72, 0.75, 0.88);
+  lhs->SetBorderSize(0);
+  lhs->SetFillStyle(0);
   
+ 
   HS_MET->Draw("HIST");
+  HS_MET->GetXaxis()->SetTitle("MET");
+  HS_MET->GetYaxis()->SetTitle("Events");
+  lhs->Clear();
+  lhs->AddEntry(H_MET[0], "#gamma+jet", "F");
+  lhs->AddEntry(H_MET[1], "Wg#rightarrowl#nug", "F");
+  lhs->AddEntry(H_MET[2], "Zg#rightarrowllg", "F");
+  lhs->AddEntry(H_MET[3], "Zg#rightarrow#nu#nug", "F");
+  lhs->Draw("SAME");
   title = HS_MET->GetName();
   c1->SaveAs(Form("%s/%s.pdf", saveto, title));
   
   //HS_dphoMETPhi->Draw("HIST");
-  HS_dphoMETPhi->SetMinimum(400);
+  
   HS_dphoMETPhi->Draw("HIST");
+  HS_dphoMETPhi->SetMinimum(400);
+  HS_dphoMETPhi->GetXaxis()->SetTitle("#Delta#phi(#gamma,MET)");
+  HS_dphoMETPhi->GetYaxis()->SetTitle("Events");
+  
+  lhs->Clear();
+  lhs->AddEntry(H_dphoMETPhi[0], "#gamma+jet", "F");
+  lhs->AddEntry(H_dphoMETPhi[1], "Wg#rightarrowl#nug", "F");
+  lhs->AddEntry(H_dphoMETPhi[2], "Zg#rightarrowllg", "F");
+  lhs->AddEntry(H_dphoMETPhi[3], "Zg#rightarrow#nu#nug", "F");
+  lhs->Draw("SAME");
   title = HS_dphoMETPhi->GetName();
   c1->SaveAs(Form("%s/%s.pdf", saveto, title));
 
+  
   HS_phoEB_ptoverMET->Draw("HIST");
+  HS_phoEB_ptoverMET->GetXaxis()->SetTitle("P_{T}^{#gamma}/MET");
+  HS_phoEB_ptoverMET->GetYaxis()->SetTitle("Events");
+  lhs->Clear();
+  lhs->AddEntry(H_phoEB_ptoverMET[0], "#gamma+jet", "F");
+  lhs->AddEntry(H_phoEB_ptoverMET[1], "Wg#rightarrowl#nug", "F");
+  lhs->AddEntry(H_phoEB_ptoverMET[2], "Zg#rightarrowllg", "F");
+  lhs->AddEntry(H_phoEB_ptoverMET[3], "Zg#rightarrow#nu#nug", "F");
+  lhs->Draw("SAME");
   //HS_phoEB_ptoverMET->GetYaxis()->SetRangeUser(0.01, 100);
   title = HS_phoEB_ptoverMET->GetName();
   c1->SaveAs(Form("%s/%s.pdf", saveto, title)); 
@@ -168,7 +200,16 @@ void xDrawStack(){
   c1->SetLogy(0);
   c1->Update();
   //HS_phoEB_pt_MET->SetMaximum(100000);
+  
   HS_phoEB_pt_MET->Draw("HIST");
+  HS_phoEB_pt_MET->GetXaxis()->SetTitle("P_{T}^{#gamma}");
+  HS_phoEB_pt_MET->GetYaxis()->SetTitle("Events");
+  lhs->Clear();
+  lhs->AddEntry(H_phoEB_pt_MET[0], "#gamma+jet", "F");
+  lhs->AddEntry(H_phoEB_pt_MET[1], "Wg#rightarrowl#nug", "F");
+  lhs->AddEntry(H_phoEB_pt_MET[2], "Zg#rightarrowllg", "F");
+  lhs->AddEntry(H_phoEB_pt_MET[3], "Zg#rightarrow#nu#nug", "F");
+  lhs->Draw("SAME");
   title = HS_phoEB_pt_MET->GetName();
   c1->SaveAs(Form("%s/%s.pdf", saveto, title));
   
