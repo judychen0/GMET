6381c12dcc0954d2e3d9ef059f89ced45126f1cd
Judy Chen <judy453378@gmail.com>
2021-11-25 12:15:27 +0800

    latest update for running skimmed trees with xZg1.C

--- runGG/myrun_data.py
+++ runGG/myrun_data.py
@@ -2,39 +2,49 @@
 import os, string
 import sys
 
-#remotepath = raw_input("Data path : ")
-#if len(sys.argv) <= 1:
-#    print("1 argument required!")
-#    remotepath = raw_input("Enter data path : ")
-#else:
-#    remotepath = sys.argv[1]
-#storepath = raw_input("Output path : ")
-
-#remotepath = raw_input("Data path(enter the mc/data path list filename HERE) : ")
 yearrun = raw_input("Year of run : ")
 isDcount = raw_input("Is Double counting? (T==1; F==0) : ")
 isData = raw_input("Is Data? (T==1; F==0) : ")
 isSignal = raw_input("Is Signal? (T==1; F==0) : ")
 
+#if int(yearrun) == 2016:
+#    if int(isDcount) == 1: remotepath = "summer16_VJet.txt"
+#    if int(isData) == 1: remotepath = "summer16_data.txt"
+#    if int(isSignal) == 1: remotepath = "summer16_mc.txt"
+#    if int(isDcount) == 0 and int(isData) == 0 and int(isSignal) == 0: remotepath = "summer16_bkg.txt"
+#    yeardir = "summer16"
+#elif int(yearrun) == 2017:
+#    if int(isDcount) == 1: remotepath = "fall17_VJet.txt"
+#    if int(isData) == 1: remotepath = "fall17_data.txt"
+#    if int(isSignal) == 1: remotepath = "fall17_mc.txt"
+#    if int(isDcount) == 0 and int(isData) == 0 and int(isSignal) == 0: remotepath = "fall17_bkg.txt"
+#    yeardir = "fall17"
+#elif int(yearrun) == 2018:
+#    if int(isDcount) == 1: remotepath = "autumn18_VJet.txt"
+#    if int(isData) == 1: remotepath = "autumn18_data.txt"
+#    if int(isSignal) == 1: remotepath = "autumn18_mc.txt"
+#    if int(isDcount) == 0 and int(isData) == 0 and int(isSignal) == 0: remotepath = "autumn18_bkg.txt"
+#    yeardir = "autumn18"
+#
 if int(yearrun) == 2016:
     if int(isDcount) == 1: remotepath = "summer16_VJet.txt"
-    if int(isData) == 1: remotepath = "summer16_data.txt"
+    if int(isData) == 1: remotepath = "./legacypath/summer16_data.txt"
     if int(isSignal) == 1: remotepath = "summer16_mc.txt"
     if int(isDcount) == 0 and int(isData) == 0 and int(isSignal) == 0: remotepath = "summer16_bkg.txt"
     yeardir = "summer16"
 elif int(yearrun) == 2017:
     if int(isDcount) == 1: remotepath = "fall17_VJet.txt"
-    if int(isData) == 1: remotepath = "fall17_data.txt"
+    if int(isData) == 1: remotepath = "./legacypath/fall17_data.txt"
     if int(isSignal) == 1: remotepath = "fall17_mc.txt"
     if int(isDcount) == 0 and int(isData) == 0 and int(isSignal) == 0: remotepath = "fall17_bkg.txt"
     yeardir = "fall17"
 elif int(yearrun) == 2018:
     if int(isDcount) == 1: remotepath = "autumn18_VJet.txt"
-    if int(isData) == 1: remotepath = "autumn18_data.txt"
+    if int(isData) == 1: remotepath = "./legacypath/autumn18_data.txt"
     if int(isSignal) == 1: remotepath = "autumn18_mc.txt"
     if int(isDcount) == 0 and int(isData) == 0 and int(isSignal) == 0: remotepath = "autumn18_bkg.txt"
     yeardir = "autumn18"
-    
+
 with open(remotepath, "r") as filepath:
     path = filepath.read().splitlines()
     for line in path:
@@ -65,8 +75,8 @@ with open(remotepath, "r") as filepath:
         script += "count=$(ls $datadir | wc -l) \n"
         script += """echo "There are tot $count ggtrees!!!" \n"""
         script += "\n"
-        script += """/usr/bin/time parallel --progress --jobs 1 'root -L -l -b -q "xZg1.C+(\\\"{1}\\\")"' :::: path.txt :::+ 1 \n"""
-        script += """/usr/bin/time parallel --progress --jobs 10 'root -L -l -b -q "xZg1.C+(\\\"{1}\\\")"' :::: path.txt \n"""
+        script += """/usr/bin/time parallel --progress --jobs 1 'root -L -l -b -q "xZgdata1.C+(\\\"{1}\\\")"' :::: path.txt :::+ 1  \n"""
+        script += """/usr/bin/time parallel --progress --jobs 10 'root -L -l -b -q "xZgdata1.C+(\\\"{1}\\\")"' :::: path.txt  \n"""
         script += "echo Processed all $count ggtrees!!! \n"
         script += "for i in {0..9}; do hadd output_$i.root output_ggtree_*$i.root; done \n"
         script += "rm -r *ggtree*  \n"
@@ -75,11 +85,10 @@ with open(remotepath, "r") as filepath:
         script += "thiswork=$(pwd) \n"
         script += "cd $workdir; rm -r $thiswork \n"
         
-        frun = open("run"+str(yeardir)+"_Zg_"+str(isDcount)+"_"+str(isData)+"_"+str(isSignal)+".sh", "w")
+        frun = open("run"+str(yeardir)+"_Zgdata_"+str(isDcount)+"_"+str(isData)+"_"+str(isSignal)+".sh", "w")
         frun.write(script)
         frun.close()
         
-        os.system( "chmod 744 run"+str(yeardir)+"_Zg_"+str(isDcount)+"_"+str(isData)+"_"+str(isSignal)+".sh" )
-        os.system( "./run"+str(yeardir)+"_Zg_"+str(isDcount)+"_"+str(isData)+"_"+str(isSignal)+".sh")
-
+        os.system( "chmod 744 run"+str(yeardir)+"_Zgdata_"+str(isDcount)+"_"+str(isData)+"_"+str(isSignal)+".sh" )
+        os.system( "./run"+str(yeardir)+"_Zgdata_"+str(isDcount)+"_"+str(isData)+"_"+str(isSignal)+".sh")
 
